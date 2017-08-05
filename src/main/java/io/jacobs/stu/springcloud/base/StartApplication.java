3eb4b9524b2dd877decaaeba6deb1efa4f5228b1
Lei Jun <leijun@lifang.com>
2017-08-05 21:45:22 +0800

    spring boot hello provider cluster config,adding register to eureka

--- src/main/java/io/jacobs/stu/springcloud/base/StartApplication.java
+++ src/main/java/io/jacobs/stu/springcloud/base/StartApplication.java
@@ -2,8 +2,11 @@ package io.jacobs.stu.springcloud.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StartApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/jacobs/stu/springcloud/base/controller/HelloController.java
+++ src/main/java/io/jacobs/stu/springcloud/base/controller/HelloController.java
@@ -1,5 +1,8 @@
 package io.jacobs.stu.springcloud.base.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/hi")
     public String sayHello(){
