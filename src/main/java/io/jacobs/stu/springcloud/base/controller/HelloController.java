fc90508eee01cba9e7ab90fa4e855d0bbb280d04
Lei Jun <leijun@lifang.com>
2017-08-07 17:51:57 +0800

    update helloService's sleep time for request timeout

--- src/main/java/io/jacobs/stu/springcloud/base/controller/HelloController.java
+++ src/main/java/io/jacobs/stu/springcloud/base/controller/HelloController.java
@@ -1,10 +1,14 @@
 package io.jacobs.stu.springcloud.base.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by Jacobs on 2017/8/5.
@@ -12,11 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private  Logger logger = Logger.getLogger(HelloController.class);
+
     @Autowired
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/hi")
-    public String sayHello(){
+    public String sayHello() throws InterruptedException {
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+
+        //添加休眠时间，导致客户端超时，触发circuit breaker逻辑
+        int sleepTimes = new Random().nextInt(3000);
+        Thread.sleep(sleepTimes);
+
+        logger.info("service_id is " + instance.getServiceId());
+
         return "hello Jacobs";
     }
 
